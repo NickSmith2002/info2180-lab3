4f836b2781759db00591d6ea591af59068169cf6
Nicholas Smith <nicholaswsmith2002@gmail.com>
2023-10-22 13:28:37 -0500

    Exercise 2 - Add X or O

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,10 +2,33 @@ window.onload = function () {
     // Exercise 1 - Layout Board
     let board = document.getElementById('board'); 
     let squares = board.getElementsByTagName('div');
-
+    let statusbox = document.getElementById('status'); 
+    let boardState = ["", "", "", "", "", "", "", "", ""]; 
+    let playerX = true; 
+    let btn = document.querySelector('.btn'); 
+    let winner = false;
+  
     for (let i = 0; i < 9; i++) {
-        let square = squares[i];
+      let square = squares[i];
+  
+      square.classList.add('square');
     
-        square.classList.add('square');
+    //Exercise 2- Add an X or O to square when clicked
+      square.addEventListener('click', function () {
+        if (!boardState[i] && !winner) {
+          if (playerX) {
+            square.textContent = 'X';
+            square.classList.add('X');
+            boardState[i] = 'X';
+          } else {
+            square.textContent = 'O';
+            square.classList.add('O');
+            boardState[i] = 'O';
+          }
+  
+          playerX = !playerX; // Switch players after each move
+  
+        }
+      });
     }
 }
